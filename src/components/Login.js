5f78fe7ebf595cb7f99ac13dea3670c76944bd10
Modala Swathi <swathimodala26@gmail.com>
2026-05-15 12:55:38 +0530

    Fixed production API URLs

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,7 @@ const Login = (props) => {
         setLoading(true);
         
         try {
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://blogplatform-ws0h.onrender.com/api/auth/login', {
                 method : 'POST',
                 headers : {
                     "Content-Type" : 'application/json'
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -41,7 +41,7 @@ const Signup = (props) => {
         setLoading(true);
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/createuser', {
+            const response = await fetch('https://blogplatform-ws0h.onrender.com/api/auth/createuser',{
                 method : 'POST',
                 headers : {
                     "Content-Type" : 'application/json'
